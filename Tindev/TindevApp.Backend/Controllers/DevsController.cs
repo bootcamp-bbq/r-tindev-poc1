60697629f082e62f28385fb87087b80da3c7c975
Ricardo Mauro <ricardo.demauro@gmail.com>
2019-08-18 16:02:49 -0300

    adding models

---
+++ Tindev/TindevApp.Backend/Controllers/DevsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TindevApp.Backend.Controllers
+{
+    public class DevsController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+
+
+        public IActionResult Like()
+        {
+            return Ok();
+        }
+
+        public IActionResult Deslike()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Tindev/TindevApp.Backend/Models/Developer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TindevApp.Backend.Models
+{
+    public class Developer
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Username { get; set; }
+
+        public string Bio { get; set; }
+
+        public string AvatarUri { get; set; }
+
+        public string GithubUri { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime ModifiedAt { get; set; }
+    }
+}
