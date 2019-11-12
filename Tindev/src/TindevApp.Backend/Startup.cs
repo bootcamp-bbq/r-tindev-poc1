714d6a218ce431b93ece2e079f70217f576fccaa
rmauro <ricardo.demauro@gmail.com>
2019-11-12 19:54:35 -0300

    adding feature manager

--- Tindev/src/TindevApp.Backend/Startup.cs
+++ Tindev/src/TindevApp.Backend/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Options;
+using Microsoft.FeatureManagement;
 using Microsoft.IdentityModel.Tokens;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.IdGenerators;
@@ -155,6 +156,8 @@ namespace TindevApp.Backend
             {
                 opts.SwaggerDoc("v1", new Info() { Title = "TinDev App", Version = "v1" });
             });
+
+            services.AddFeatureManagement();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
